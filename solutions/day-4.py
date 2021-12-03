c45185d1a6d8d9a6888f49262668ab192080d09b
Parth Sarin <psarin@stanford.edu>
2021-12-03 14:51:52 -0800

    Day 4 template

---
+++ solutions/day-4.py
@@ -0,0 +1,27 @@
+"""
+File: day-4.py
+--------------
+For Level 1, use the website. For Level 2, use the file puzzles/day-...
+"""
+import os
+from utils import *
+
+data = get_input(write = True, day=4, year=2021)
+
+# --- Part 1 ---
+ans_1 = None
+
+# --- Part 2 ---
+ans_2 = None
+
+# --- Submission Code ---
+if not os.path.exists(f'../puzzles/day-4.txt'):
+    # Level 2 hasn't been generated yet; we're on level 1
+    correct = submit(ans_1, level=1, day=4, year=2021)
+    if correct:
+        puzzle = get_puzzle()
+        with open(f'../puzzles/day-4.txt', 'w') as f:
+            f.write(puzzle)
+else:
+    # Level 2 has been generated; we're on level 2
+    correct = submit(ans_2, level=2, day=4, year=2021)
